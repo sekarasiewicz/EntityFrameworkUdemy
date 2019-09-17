dcbcc5ff2ccdd1d42022147d4fea56d03cea7699
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2019-09-17 22:50:20 +0200

    Init

---
+++ EntityFrameworkUdemy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityFrameworkUdemy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
