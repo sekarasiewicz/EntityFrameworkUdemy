139bf1c68747e4adfc24325e82f8b01210a715ec
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2019-09-18 22:21:02 +0200

    Add category relation remove useless dependency

--- EntityFrameworkUdemy/Course.cs
+++ EntityFrameworkUdemy/Course.cs
@@ -12,5 +12,7 @@ namespace EntityFrameworkUdemy
         public Author Author { get; set; }
 //        public ICollection<Tag> Tags { get; set; }
         public List<CourseTag> CourseTags { get; set; }
+        
+        public Category Category { get; set; }
     }
 }
---
+++ EntityFrameworkUdemy/Migrations/20190918200400_AddCategoryColumnToCoursesTable.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityFrameworkUdemy.Migrations
+{
+    public partial class AddCategoryColumnToCoursesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Courses",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_CategoryId",
+                table: "Courses",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_Categories_CategoryId",
+                table: "Courses",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_Categories_CategoryId",
+                table: "Courses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_CategoryId",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Courses");
+        }
+    }
+}
