30ca4c735c9500197646c0cb3b224d4e04a31146
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2019-09-18 11:35:11 +0200

    move all classes to separate classes

---
+++ EntityFrameworkUdemy/PlutoContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkUdemy
+{
+    public class PlutoContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+//        protected PlutoContext() : base("name=DefaultConnection")
+//                 {
+//        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CourseTag>()
+                .HasKey(ct => new { ct.CourseId, ct.TagId });
+
+            modelBuilder.Entity<CourseTag>()
+                .HasOne(ct => ct.Course)
+                .WithMany(c => c.CourseTags)
+                .HasForeignKey(ct => ct.CourseId);
+
+            modelBuilder.Entity<CourseTag>()
+                .HasOne(ct => ct.Tag)
+                .WithMany(t => t.CourseTags)
+                .HasForeignKey(ct => ct.TagId);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=127.0.0.1;Database=CodeFirstDemo;User Id=sa;Password=Your_password123");
+        }
+    }
+}
---
+++ EntityFrameworkUdemy/Tag.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace EntityFrameworkUdemy
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+//        public ICollection<Course> Courses{ get; set; }
+        public List<CourseTag> CourseTags { get; set; }
+    }
+}
