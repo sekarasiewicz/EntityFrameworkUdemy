3e2aec8226193df282950b741a9a6fbedc9ed12a
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2019-09-25 23:20:58 +0200

    annotations

--- EntityFrameworkUdemy/Category.cs
+++ EntityFrameworkUdemy/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace EntityFrameworkUdemy
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntityFrameworkUdemy
 {
     public class Category
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(255)]
         public string Name { get; set; }
     }
 }
---
+++ EntityFrameworkUdemy/Migrations/20190920201944_AddCategoryNameConstrains.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityFrameworkUdemy.Migrations
+{
+    public partial class AddCategoryNameConstrains : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255);
+        }
+    }
+}
