93dae880137eee7a455c286f6eee04b4a3b6f3b2
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2019-09-29 18:47:51 +0200

    first join

--- EntityFrameworkUdemy/Program.cs
+++ EntityFrameworkUdemy/Program.cs
@@ -9,7 +9,7 @@ namespace EntityFrameworkUdemy
         static void Main(string[] args)
         {
             var context = new PlutoContext();
-            
+
             // LINQ syntax
             var query = from c in context.Courses
                 where c.Name.Contains("c#")
@@ -20,16 +20,24 @@ namespace EntityFrameworkUdemy
             {
                 Console.WriteLine(course.Name);
             }
-            
+
             // Extension methods
-            var courses = context.
-                Courses.
-                Where(c => c.Name.Contains("c#")).
-                OrderBy(c => c.Name);
+            var courses = context.Courses.Where(c => c.Name.Contains("c#")).OrderBy(c => c.Name);
 
             foreach (var course in courses)
             {
                 Console.WriteLine("COURSE - {0}", course.Name);
+            }
+
+            var queryable = context.Courses.Join(context.Authors, c => c.AuthorId, a => a.Id, (course, author) => new
+            {
+                CourseName = course.Name,
+                AuthorName = author.Name
+            });
+
+            foreach (var q in queryable)
+            {
+                Console.WriteLine(q);
             }
         }
     }
